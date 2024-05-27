fab19f7dbf39a154e5d185140dc4c0d1b2a72844
AlonLandmann <rafael.landmann@gmail.com>
2024-05-27 17:12:44 +0800

    streets working

--- texas/texas-streets.py
+++ texas/texas-streets.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 # training parameters
-NUM_ITERATIONS = 10000000
+NUM_ITERATIONS = 100000
 
 # game parameters
 N = 3
 CARDS = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'][:N]
-STACK_SIZE = 20
+STACK_SIZE = 4
 
 # utilities
 def card_value(card):
@@ -50,15 +50,12 @@ def node_type(history):
   if len(history) <= 1:
     return 'player'
   elif history[-1] == 'f':
-    return 'fold'
+    return 'folded'
   elif is_raise(history[-2]) and history[-1] == 'c':
     return 'showdown' if is_river(history) else 'chance'
   elif history[-2] == 'c' and history[-1] == 'c':
     return 'showdown' if is_river(history) else 'chance'
   return 'player'
-
-def is_terminal(history):
-  return node_type(history) in ['fold', 'showdown']
 
 def get_player(history):
   last_street = history.copy()
@@ -80,15 +77,12 @@ def get_low_and_high(history):
       break
   return (low, high)
 
-def get_payoff(history, cards):
+def get_payoff(history, ntype, player, cards):
   low, high = get_low_and_high(history)
-  player = get_player(history)
-  type = node_type(history)
-  if type == 'fold':
+  assert ntype in ['folded', 'showdown']
+  if ntype == 'folded':
     return low * (-1 if player == 0 else 1)
-  elif type == 'showdown':
-    return  high * showdown_multiplier(cards)
-  return None
+  return  high * showdown_multiplier(cards)
 
 def facing_check_or_new(stakes):
   actions = ['c']
@@ -98,9 +92,10 @@ def facing_check_or_new(stakes):
 
 def facing_raise(stakes, raise_size):
   actions = ['f', 'c']
-  for a in range(stakes + raise_size, STACK_SIZE):
-    actions.append(a)
-  actions.append(STACK_SIZE)
+  if stakes < STACK_SIZE:
+    for a in range(stakes + raise_size, STACK_SIZE):
+      actions.append(a)
+    actions.append(STACK_SIZE)
   return actions
 
 def get_actions(history):
@@ -114,92 +109,123 @@ def get_actions(history):
     elif is_raise(last):
       return facing_raise(high, high - low)
   return None
-    
 
+def unique_key(history, player, cards):
+  key = cards[player]
+  for a in history:
+    key += ',' + str(a)
+  return key
+
+# information sets
+class InformationSet:
+  def __init__(self, actions):
+    n = len(actions)
+    self.actions = actions
+    self.strategy = np.ones(n) / n
+    self.strategy_sum = np.zeros(n)
+    self.regret_sum = np.zeros(n)
+  
+  def update_strategy(self, realization_weight):
+    n = len(self.actions)
+    normalizing_sum = 0
+    for a in range(n):
+      self.strategy[a] = max(self.regret_sum[a], 0)
+      normalizing_sum += self.strategy[a]
+    for a in range(n):
+      if normalizing_sum > 0:
+        self.strategy[a] /= normalizing_sum
+      else:
+        self.strategy[a] = 1.0 / n
+      self.strategy_sum[a] += realization_weight * self.strategy[a]
+    return self.strategy
+  
+  def get_average_strategy(self):
+    n = len(self.actions)
+    normalizing_sum = sum(self.strategy_sum)
+    if normalizing_sum > 0:
+      return self.strategy_sum / normalizing_sum
+    else:
+      return np.ones(n) / n
+
+# cfr algorithm
+class CfrTrainer:
+  def __init__(self):
+    self.node_map = {}
+  
+  def train(self):
+    utility = 0
+    for k in range(NUM_ITERATIONS):
+      cards = deal_cards()
+      utility += self.cfr(cards, [], 1, 1)
+      if k % (NUM_ITERATIONS // 100) == 0:
+        print(f'{100 * k / NUM_ITERATIONS:.2f}%')
+    return utility / NUM_ITERATIONS
+  
+  def get_strategy(self):
+    strategies = {}
+    for key, info_set in self.node_map.items():
+      strategies[key] = {
+        'actions': info_set.actions,
+        'strategy': info_set.get_average_strategy()
+      }
+    return strategies
+
+  def cfr(self, cards, history, p0, p1):
+    # determine the node type
+    ntype = node_type(history)
+    
+    # determine whose turn it is
+    player = get_player(history)
+    
+    # return the payoff at a terminal node
+    if ntype in ['folded', 'showdown']:
+      return get_payoff(history, ntype, player, cards)  * (-1 if player == 0 else 1)
+    
+    # make sure we are at a player node
+    assert ntype == 'player'
+    
+    # create and / or select information set
+    key = unique_key(history, player, cards)
+    if not key in self.node_map:
+      self.node_map[key] = InformationSet(get_actions(history))
+    info_set = self.node_map[key]
+    
+    # update strategy and the sum, and retrieve strategy to work with it
+    strategy = info_set.update_strategy(p0 if player == 0 else p1)
+    
+    # define variables for recursive call and regret update
+    n = len(strategy)
+    utilities = np.zeros(n)
+    node_utility = 0
+    
+    # recursive call
+    for a in range(n):
+      next_history = history + [info_set.actions[a]]
+      if node_type(next_history) == 'chance':
+        next_history = next_history + [cards[2]]
+      if player == 0:
+        utilities[a] = -self.cfr(cards, next_history, p0 * strategy[a], p1)
+      else:
+        utilities[a] = -self.cfr(cards, next_history, p0, p1 * strategy[a])
+      node_utility += strategy[a] * utilities[a]
+    
+    # update regret
+    for a in range(n):
+      regret = utilities[a] - node_utility
+      info_set.regret_sum[a] += (p1 if player == 0 else p0) * regret
+    
+    # return expected node utility
+    return node_utility
+
+  
 # script
-# cards = deal_cards()
-# print(cards)
-
-# h = [
-#   [],
-#   ['c'],
-#   ['c', 4],
-#   ['c', 4, 'c'],
-#   ['c', 4, 'c', cards[2]],
-#   ['c', 4, 'c', cards[2], 7],
-#   ['c', 4, 'c', cards[2], 7, 12],
-#   ['c', 4, 'c', cards[2], 7, 12, 'f'],
-#   ['c', 4, 'c', cards[2], 7, 12, 'c'],
-#   ['c', 4, 'c', cards[2], 7, 12, 17],
-#   ['c', 4, 'c', cards[2], 7, 12, 17, 'f'],
-#   ['c', 4, 'c', cards[2], 7, 12, 17, 'c']
-# ]
-
-# for history in h:
-#   print(history)
-#   print(f'is river: {is_river(history)}')
-#   print(f'get board: {get_board(history)}')
-#   print(f'node type: {node_type(history)}')
-#   print(f'is_terminal: {is_terminal(history)}')
-#   print(f'get_player: {get_player(history)}')
-#   print(f'get_payoff: {get_payoff(history, cards)}')
-#   print(f'get_actions: {get_actions(history)}')
+trainer = CfrTrainer()
+ev = trainer.train()
+strategies = trainer.get_strategy()
+print(ev)
+for strat in strategies:
+  print(strat, strategies[strat])
+  
     
 
-# # information sets
-# class InformationSet:
-#   def __init__(self, actions):
-#     n = len(actions)
-#     self.actions = actions
-#     self.strategy = np.ones(n) / n
-#     self.strategy_sum = np.zeros(n)
-#     self.regret_sum = np.zeros(n)
-  
-#   def update_strategy(self, realization_weight):
-#     n = len(self.actions)
-#     normalizing_sum = 0
-#     for a in range(n):
-#       self.strategy[a] = max(self.regret_sum[a], 0)
-#       normalizing_sum += self.strategy[a]
-#     for a in range(n):
-#       if normalizing_sum > 0:
-#         self.strategy[a] /= normalizing_sum
-#       else:
-#         self.strategy[a] = 1.0 / n
-#       self.strategy_sum[a] += realization_weight * self.strategy[a]
-#     return self.strategy
-  
-#   def get_average_strategy(self):
-#     n = len(self.actions)
-#     normalizing_sum = sum(self.strategy_sum)
-#     if normalizing_sum > 0:
-#       return self.strategy_sum / normalizing_sum
-#     else:
-#       return np.ones(n) / n
 
-# # cfr algorithm
-# class CfrTrainer:
-#   def __init__(self):
-#     self.node_map = {}
-  
-#   def train(self):
-#     utility = 0
-#     for k in range(NUM_ITERATIONS):
-#       cards = deal_cards()
-#       utility += self.cfr(cards, '', 1, 1)
-#       if k % (NUM_ITERATIONS // 100) == 0:
-#         print(f'{100 * k / NUM_ITERATIONS:.2f}%')
-#     return utility / NUM_ITERATIONS
-  
-#   def get_strategy(self):
-#     strategies = {}
-#     for key, info_set in self.node_map.items():
-#       strategies[key] = {
-#         'actions': info_set.actions,
-#         'strategy': info_set.get_average_strategy()
-#       }
-#     return strategies
-  
-#   def cfr(self, cards, history, p0, p1):
-#     # determine whose turn it is
-#     player = determine_turn(history)
